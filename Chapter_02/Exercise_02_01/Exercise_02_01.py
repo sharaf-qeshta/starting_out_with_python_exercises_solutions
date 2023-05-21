52229b4bae3081012dffb14f1dc5d6097727201a
sharaf-qeshta <sharaf.keshta@gmail.com>
2023-05-21 08:58:08 -0700

    Personal Information

---
+++ Chapter_02/Exercise_02_01/Exercise_02_01.py
@@ -0,0 +1,18 @@
+"""
+1. Personal Information
+Write a program that displays the following information:
+• Your name
+•	 Your address, with city, state, and ZIP
+• Your telephone number
+• Your college major
+
+@author Sharaf Qeshta
+"""
+
+print("Name:", "your name")
+print("Address:", "your address")
+print("City:", "your city")
+print("State:", "your state")
+print("ZIP:", "your zip")
+print("Telephone Number:", "your telephone number")
+print("Major:", "your major")
