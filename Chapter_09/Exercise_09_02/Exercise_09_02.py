029fcc92483331de7193711353215a83e07ff556
Sharaf Qeshta <sharaf.keshta@gmail.com>
2023-11-12 14:39:36 -0500

    Capital Quiz

---
+++ Chapter_09/Exercise_09_02/Exercise_09_02.py
@@ -0,0 +1,78 @@
+"""
+2. Capital Quiz
+Write a program that creates a dictionary containing the U.S. states as keys, and their capitals
+as values. (Use the Internet to get a list of the states and their capitals.) The program
+should then randomly quiz the user by displaying the name of a state and asking the user
+to enter that state’s capital. The program should keep a count of the number of correct and
+incorrect responses. (As an alternative to the U.S. states, the program can use the names of
+countries and their capitals.)
+
+@author Sharaf Qeshta
+"""
+
+STATES = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinios': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Monies',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'St. Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Neveda': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhoda Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakoda': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne'
+}
+
+
+def main():
+    correct, incorrect = 0, 0
+    for key, value in STATES.items():
+        answer = input(f"what is the capital of {key}: ")
+        if value == answer:
+            correct += 1
+        else:
+            incorrect += 1
+    print(f"you answered {correct} questions right and {incorrect} questions wrong.")
+
+
+main()
